fb281144fd9d5731e8b015ab327988f8e1fb6a1c
Albert Witwerts <albertwitwerts@msn.com>
2020-06-01 23:44:06 +0200

    SPA setup

---
+++ src/scripts/controllers.js
@@ -0,0 +1,8 @@
+﻿var controllers = angular.module('controllers', []);
+
+controllers.controller("agendaController",
+    function Controller($scope, $http) {
+        $http.get('data/appointments.json').then(function(response) {
+            $scope.appointments = response.data;
+        });
+});
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,38 @@
-﻿
+﻿var gpApp = angular.module("gpApp", [
+    'ui.router',
+    'controllers'
+]);
+
+gpApp.config(['$stateProvider', function($stateProvider, $urlRouterProvider){
+    $stateProvider
+        .state('root', {
+            url: '',
+            views: {
+                'header@root': {
+                    templateUrl: 'template/header.html'
+                },
+                '@': {
+                    templateUrl: 'template/pages/agenda.html',
+                    controller: 'agendaController'
+                }, 
+                'footer@root': {
+                    templateUrl: 'template/footer.html'
+                }
+            }
+        })
+        .state('settings', {
+                url: '/settings',
+                views: {
+                    'header@settings': {
+                        templateUrl: 'template/footer.html'
+                    },
+                    '@': {
+                        templateUrl: 'template/pages/agenda.html',
+                        controller: 'agendaController'
+                    },
+                    'footer@settings': {
+                        templateUrl: 'template/header.html'
+                    }
+                }
+            })
+}])
